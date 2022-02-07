9e99ee19b1a93db3d859f6c1d589ab2ed5088dfa
Osama_mousa <osamamousa048@gmail.com>
2022-02-07 09:37:25 -0500

    Mayhapseagoodtry

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ buttons.map( button => {
                 break;
             case '←':
                 if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
+                    display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
             default:
